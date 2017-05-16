422ad470a8c2554216c32ae21aa79a579a16ac09
Vladimir <originalblack@mail.ru>
2017-05-17 00:28:22 +0300

    add one more test codes

---
+++ test2.js
@@ -0,0 +1,31 @@
+var createPet = function(name) {
+  var sex;
+  
+  return {
+    setName: function(newName) {
+      name = newName;
+    },
+    
+    getName: function() {
+      return name;
+    },
+    
+    getSex: function() {
+      return sex;
+    },
+    
+    setSex: function(newSex) {
+      if(typeof newSex === 'string' && (newSex.toLowerCase() === 'male' || newSex.toLowerCase() === 'female')) {
+        sex = newSex;
+      }
+    }
+  }
+}
+
+var pet = createPet('Vivie');
+pet.getName();                 
+
+pet.setName('Oliver');
+pet.setSex('male');
+pet.getSex();                  e
+pet.getName();                  
